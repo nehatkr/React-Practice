8f5edf8e0a6a1d77eec44b34bfae66660163cbef
Nehatkr <nehathakur2308@gmail.com>
2025-12-04 23:42:28 +0530

    data of the user

---
+++ src/data/UserData.jsx
@@ -0,0 +1,28 @@
+const userData = [
+  {
+    id: 1,
+    name: "Neha Kumari",
+    age: 22,
+    email: "neha@example.com",
+    interests: ["Drawing", "Chess", "Reading Books"],
+    isActive: true
+  },
+  {
+    id: 2,
+    name: "Rahul Sharma",
+    age: 25,
+    email: "rahul@example.com",
+    interests: ["Cricket", "Movies", "Coding"],
+    isActive: false
+  },
+  {
+    id: 3,
+    name: "Aditi Verma",
+    age: 23,
+    email: "aditi@example.com",
+    interests: ["Design", "Travel", "Music"],
+    isActive: true
+  }
+];
+
+export default userData;
